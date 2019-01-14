05cfafa1369fec183086615903a544437eb9840c
shun <>
2019-01-14 20:04:25 +0900

    ビット列の計算のテンプレを追加

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,50 +1,43 @@
 <template>
   <div id="app">
-                <div class="level">
-                <div class="level-item title has-text-grey-darker">
-                    {{title}}
-                </div>
-            </div>
-
-            <div class="level">
-                <div class="level-item title has-text-grey-darker">
-                    <div class="field has-addons">
-                        <p class="control">
-                            <a class="button is-info is-selected">
-                                <span>乗数</span>
-                            </a>
-                        </p>
-                        <p class="control">
-                            <a class="button">
-                                <span>ビット</span>
-                            </a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-    <multi/>
+    <div class="level">
+      <div class="level-item title has-text-grey-darker">{{title}}</div>
+    </div>
+       <b-tabs position="is-centered" class="block">
+            <b-tab-item label="乗数"><multi /></b-tab-item>
+            <b-tab-item label="ビット"><binary /></b-tab-item>
+        </b-tabs>
   </div>
 </template>
 
 <script>
-import multi from './components/multi.vue'
+import multi from "./components/multi.vue";
+import binary from "./components/binary.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    multi
+    multi,
+    binary
   },
-  data(){
-    return{
-      title:'計算ドリル'
+  data() {
+    return {
+      title: "計算ドリル",
+      currentTab: 'multi',
+      tabs: ['multi','binary']
+    };
+  },
+  computed:{
+    currentTabComponent:function(){
+      return 'tab-'+this.currentTab.toLowerCase()
     }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
